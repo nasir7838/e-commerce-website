e1d9fe19c6e852c0c7ea44255dea4f0a55949520
nasir7838 <nasiralam7838@gmail.com>
2025-07-22 23:31:02 +0530

    Delete tokenGenerate.js

--- tokenGenerate.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const generateToekn = (id) =>{
-    return jwt.sign({id}, process.env.JWT_SECRECT,{
-        expiresIn:"30d"
-    })
-}
-
-module.exports = generateToekn;
